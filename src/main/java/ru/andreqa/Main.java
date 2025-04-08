b4b669523f6ebb4e660a2d337a94774fc291dedc
MrAndreQA <86068145+MrAndreQA@users.noreply.github.com>
2025-04-08 11:17:39 +0300

    Update Main.java

--- src/main/java/ru/andreqa/Main.java
+++ src/main/java/ru/andreqa/Main.java
@@ -2,8 +2,8 @@ package ru.andreqa;
 
 public class Main {
     public static void main(String[] args) {
-        String c = "Я ";
-        String b = "Успел первым изменить";
-        System.out.println(c + b);
+        String f = "I ";
+        String g = "Успел первым изменить!!";
+        System.out.println(f + g);
     }
 }
